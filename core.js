da49d166e56d5672d09cb66129b63e9523acd749
Robert Wertman <rwertman@radish.localdomain>
2011-11-16 21:42:09 -0500

    Metro.js

---
+++ core.js
@@ -0,0 +1,21 @@
+var metro = metro || {
+    config: {
+        theme: "light",
+        accent: "blue"
+    },
+    ui: {
+        themes: {
+            "light": "#FFF",
+            "dark": "#000"
+        },
+        colors: {
+            "purple": "#7C4199",
+            "pink": "#EC098C",
+            "orange": "#F37735",
+            "blue": "#00AEDB",
+            "red": "#D11141",
+            "green": "#00B159",
+            "yellow": "#FFC425"
+        }
+    }
+};
