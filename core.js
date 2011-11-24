3b994e44839e86a4b38ca4ebc167a82e57308543
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-11-24 11:04:05 -0500

    Started to format like a standard JS lib.

--- core.js
+++ core.js
@@ -1,48 +1,51 @@
-var metro = metro || {
-    config: {
-        theme: "light",
-        accent: "blue"
-    },
-    ui: {
-        themes: {
-            "light": "#FFF",
-            "dark": "#000"
-        },
-        colors: {
-            "purple": "#7C4199",
-            "pink": "#EC098C",
-            "orange": "#F37735",
-            "blue": "#00AEDB",
-            "red": "#D11141",
-            "green": "#00B159",
-            "yellow": "#FFC425"
-        },
-        control: {
-            /* Todo:
-             * Button
-             * CheckBox
-             * Grid
-             * HyperlinkButton
-             * Image
-             * InkPresenter
-             * ListBox
-             * Map
-             * MediaElement
-			 * Page
-             * Panorama
-             * PasswordBox
-             * Pivot
-             * ProgressBar
-             * RadioButton
-             * ScrollViewer
-             * Slider
-             * TextBlock
-             * TextBox
-             *
-             * ?:
-             * Stack panel
-             * Canvas
-             */
-        }
-    }
-};
+(function() {
+	var metro = window["metro"] || {
+		config: {
+			theme: "light",
+			accent: "blue"
+		},
+		ui: {
+			themes: {
+				"light": "#FFF",
+				"dark": "#000"
+			},
+			colors: {
+				"purple": "#7C4199",
+				"pink": "#EC098C",
+				"orange": "#F37735",
+				"blue": "#00AEDB",
+				"red": "#D11141",
+				"green": "#00B159",
+				"yellow": "#FFC425"
+			},
+			control: {
+				/* Todo:
+				 * Button
+				 * CheckBox
+				 * Grid
+				 * HyperlinkButton
+				 * Image
+				 * InkPresenter
+				 * ListBox
+				 * Map
+				 * MediaElement
+				 * Modal Window
+				 * Page
+				 * Panorama
+				 * PasswordBox
+				 * Pivot
+				 * ProgressBar
+				 * RadioButton
+				 * ScrollViewer
+				 * Slider
+				 * TextBlock
+				 * TextBox
+				 *
+				 * ?:
+				 * Stack panel
+				 * Canvas
+				 */
+			}
+		}
+	};
+})();
